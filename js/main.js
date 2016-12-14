4c15e4a4c47cd583c41346d2f0488733559fff16
vincent scotto <vincent@vincents-iMac.local>
2016-12-14 15:27:28 -0800

    Updated site with actual content

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$('.icon').click(function(e) {
-  e.preventDefault();
-  $(this).toggleClass('active');
-  $('.container').toggleClass('opened');
-});
-
-$('#checkbox').on('click', function() {
-  $('.greeting p span:first').toggleClass('bb');
-})
