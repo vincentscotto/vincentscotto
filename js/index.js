d9d25d1439654aaea7d4b33cd34d99334ead9f25
Vincent Scotto <vscotto@doximity.com>
2024-08-28 12:15:43 -0700

    minor updates to vanilla js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(".icon").click(function(e) {
+$(".icon").click(function (e) {
   e.preventDefault();
   $(this).toggleClass("active");
   $('.links').animate({
@@ -6,13 +6,13 @@ $(".icon").click(function(e) {
   }, 500);
 });
 
-$(window).resize(function() {
+$(window).resize(function () {
   if ($(window).width() > 641) {
     $('.links').show();
   }
 });
 
-$('#checkbox').on('click', function() {
+$('#checkbox').on('click', function () {
   if ($('#home p span.bb').is(':hidden')) {
     $('#home p span.bb').show();
     $('.working').addClass('strike');
@@ -23,64 +23,56 @@ $('#checkbox').on('click', function() {
 
 });
 
-var lastId,
-  mainMenu = $(".links"),
-  mainMenuHeight = mainMenu.outerHeight(),
-  menuItems = mainMenu.find("a"),
+const menuItems = document.querySelectorAll('.links a');
+const sections = Array.from(menuItems).map(link => document.querySelector(link.getAttribute('href')));
 
-  scrollItems = menuItems.map(function() {
-    var item = $($(this).attr("href"));
-    if (item.length) {
-      return item;
+menuItems.forEach(menuItem => {
+  menuItem.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = menuItem.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
+
+window.addEventListener('scroll', function () {
+  const fromTop = window.scrollY + window.innerHeight / 3;
+
+  let currentSection = sections.find(section => {
+    return section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop;
+  });
+
+  menuItems.forEach(menuItem => {
+    menuItem.parentElement.classList.remove('active');
+    if (currentSection && menuItem.getAttribute('href') === `#${currentSection.id}`) {
+      menuItem.parentElement.classList.add('active');
     }
   });
-
-menuItems.click(function(e) {
-  var href = $(this).attr("href"),
-    offsetTop = href === "#" ? 0 : $(href).offset().top - 35;
-  $('html, body').stop().animate({
-    scrollTop: offsetTop
-  }, 1000);
-  e.preventDefault();
 });
 
-$(window).scroll(function() {
-  var fromTop = $(this).scrollTop() + mainMenuHeight;
 
-  var cur = scrollItems.map(function() {
-    if ($(this).offset().top < fromTop)
-      return this;
-  });
-
-  cur = cur[cur.length - 1];
-  var id = cur && cur.length ? cur[0].id : "";
-
-  if (lastId !== id) {
-    lastId = id;
-
-    menuItems
-      .parent().removeClass("active")
-      .end().filter("[href='#" + id + "']").parent().addClass("active");
-  }
-});
 
 // contact form
 var $contactForm = $('#contact-form');
-$contactForm.submit(function(e) {
+$contactForm.submit(function (e) {
   e.preventDefault();
   $.ajax({
     url: 'https://formspree.io/vincent@scotto.ws',
     method: 'POST',
     data: $(this).serialize(),
     dataType: 'json',
-    beforeSend: function() {
+    beforeSend: function () {
       $contactForm.append('<div class="alert alert--loading">Sending message...</div>');
     },
-    success: function(data) {
+    success: function (data) {
       $contactForm.find('.alert--loading').hide();
       $contactForm.append('<div class="alert alert--success">Message sent!</div>');
     },
-    error: function(err) {
+    error: function (err) {
       $contactForm.find('.alert--loading').hide();
       $contactForm.append('<div class="alert alert--error">Oops, there was an error. Please send me an email instead: <a href="mailto:vincent@scotto.ws">vincent @ scotto.ws</a>. Thanks!</div>');
     }
